c968c975d4214eb5884d304bf275165b8d38590a
Norbert Jon Bobila <norbertbobila12@gmail.com>
2024-04-10 00:11:23 +0800

    test

--- src/main/java/com/proj/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/proj/expensetracker/ExpenseTrackerApplication.java
@@ -10,7 +10,7 @@ public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
 
-
+		System.out.println("Norbs");
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
 
