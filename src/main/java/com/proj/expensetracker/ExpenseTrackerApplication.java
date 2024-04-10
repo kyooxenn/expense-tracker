b67288ad672eb51eca19ee0c12185458317f6f37
Norbert Jon Bobila <norbertbobila12@gmail.com>
2024-04-10 18:29:04 +0800

    add another

--- src/main/java/com/proj/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/proj/expensetracker/ExpenseTrackerApplication.java
@@ -11,6 +11,7 @@ public class ExpenseTrackerApplication {
 	public static void main(String[] args) {
 
 		System.out.println("remove");
+		System.out.println("add");
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
 
